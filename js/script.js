ffb0b8bbca16f5442fa09346587767d5754413fb
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-08 13:34:42 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on Mar. 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // This function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML= "<p>Hello, World!</p>";
+}
